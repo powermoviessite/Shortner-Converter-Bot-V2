9567571397c8d26b57c5c99e0a41a6c1f5b3b718
powermoviessite <130778614+powermoviessite@users.noreply.github.com>
2023-04-22 18:46:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "17193413"))
+API_HASH = os.environ.get("API_HASH", "5e50e6380252d9d487093bae4171bf91")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5958464071:AAEYQHOmyZnxQ0ksNjpT1La0GDJoh_mDNzw")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("1606667548")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "powermovies")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://powermovies:powermovies11@cluster0.je3b1k7.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "1606667548")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(Id Owned Id)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001649801629")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "@PowerMoviesBots") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
-WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", 'https://te.legra.ph/file/a8241af47262bd4f67523.jpg') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
